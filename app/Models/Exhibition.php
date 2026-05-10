c32e26bbb7161a61aea7b47d8f3a21f322f0d8e8
juda-rgb <judabenhur3104@gmail.com>
2026-05-10 11:06:15 +0700

    Add files via upload

--- app/Models/Exhibition.php
+++ app/Models/Exhibition.php
@@ -2,9 +2,40 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Exhibition extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'start_date',
+        'end_date',
+        'museum_id',
+    ];
+
+    protected $casts = [
+        'start_date' => 'date',
+        'end_date' => 'date',
+    ];
+
+    public function museum()
+    {
+        return $this->belongsTo(Museum::class);
+    }
+
+    public function artworks()
+    {
+        return $this->belongsToMany(
+            Artwork::class,
+            'exhibition_artworks'
+        );
+    }
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
